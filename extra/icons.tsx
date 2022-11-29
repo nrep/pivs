e242d55f8b49aebbf133f906b030216e7504a906
nrep <elvisrugero@gmail.com>
2022-11-30 00:54:51 +0200

    feat: :bento: Added some assets

--- extra/icons.tsx
+++ extra/icons.tsx
@@ -2,18 +2,6 @@ import React from 'react';
 import { ImageStyle } from 'react-native';
 import { Icon, IconElement } from '@ui-kitten/components';
 
-export const ArrowForwardIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='arrow-forward-outline'/>
-);
-
-export const GoogleIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='google'/>
-);
-
-export const FacebookIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='facebook'/>
-);
-
-export const TwitterIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='twitter'/>
+export const CartIcon = (style: ImageStyle): IconElement => (
+  <Icon {...style} name='shopping-cart'/>
 );
