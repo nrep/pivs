b1befd855b2f52af5b77f99b0fd05b5d31abebdf
nrep <elvisrugero@gmail.com>
2022-12-06 01:45:08 +0200

    feat: :sparkles: Added QR Code Screen

---
+++ qr-code-scanner.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+
+import { StyleSheet, Text } from 'react-native';
+import { useCameraDevices } from 'react-native-vision-camera';
+import { Camera } from 'react-native-vision-camera';
+import { useScanBarcodes, BarcodeFormat } from 'vision-camera-code-scanner';
+
+export default function QRScanner({ navigation }) {
+    const [hasPermission, setHasPermission] = React.useState(false);
+    const devices = useCameraDevices();
+    const device = devices.back;
+
+    const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE], {
+        checkInverted: true,
+    });
+
+    const onBarcodeDected = (barcode) => {
+        if (barcode.displayValue.startsWith("{") && barcode.displayValue.endsWith("}")) {
+            navigation && navigation.navigate('CreateOrder', {
+                product: JSON.parse(barcode.displayValue),
+                quantity: 1
+            });
+        }
+    }
+
+    // Alternatively you can use the underlying function:
+    //
+    // const frameProcessor = useFrameProcessor((frame) => {
+    //   'worklet';
+    //   const detectedBarcodes = scanBarcodes(frame, [BarcodeFormat.QR_CODE], { checkInverted: true });
+    //   runOnJS(setBarcodes)(detectedBarcodes);
+    // }, []);
+
+    React.useEffect(() => {
+        (async () => {
+            const status = await Camera.requestCameraPermission();
+            setHasPermission(status === 'authorized');
+        })();
+    }, []);
+
+    return (
+        device != null &&
+        hasPermission && (
+            <>
+                <Camera
+                    style={StyleSheet.absoluteFill}
+                    device={device}
+                    isActive={true}
+                    frameProcessor={frameProcessor}
+                    frameProcessorFps={5}
+                />
+                {barcodes.map((barcode, idx) => (
+                    <Text key={idx} style={styles.barcodeTextURL}>
+                        {barcode.displayValue}
+                    </Text>
+                ))}
+            </>
+        )
+    );
+}
+
+const styles = StyleSheet.create({
+    barcodeTextURL: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+});
